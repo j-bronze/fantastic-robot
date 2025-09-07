b7205347636bc24abd7b648e6932667874c6d0f1
yauheniya <evgeniya.boroznovskaya@gmail.com>
2025-09-08 01:13:53 +0300

    added task2

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+//task1
         Scanner scanner = new Scanner(System.in);
-        String startMessage = "Please enter a number from 0 to 100:";
-        String warningMessage = "Incorrect value entered. ";
+//        String startMessage = "Please enter a value from 0 to 100:";
+//        String warningMessage = "Incorrect value entered. ";
+//
+//        System.out.println(startMessage);
+//        int userInput = scanner.nextInt();
+//
+//        if (userInput < 0 || userInput > 100) {
+//            do {
+//                System.out.println(warningMessage);
+//                System.out.println(startMessage);
+//                userInput = scanner.nextInt();
+//            } while (!(userInput >= 0 && userInput <= 100));
+//        }
+//        if (userInput >= 90) {
+//            System.out.println("Your grade is A!");
+//        } else if (userInput >= 80) {
+//            System.out.println("Your grade is B!");
+//        } else if (userInput >= 70) {
+//            System.out.println("Your grade is C!");
+//        } else if (userInput >= 60) {
+//            System.out.println("Your grade is D!");
+//        } else if (userInput >= 50) {
+//            System.out.println("Your grade is E!");
+//        } else {
+//            System.out.println("Your grade is F!");
+//        }
 
-        System.out.println(startMessage);
-        int userInput = scanner.nextInt();
+//task2
+        String userValue = "";
+        //Scanner scanner = new Scanner(System.in);
 
-        if (userInput < 0 || userInput > 100) {
-            do {
-                System.out.println(warningMessage);
-                System.out.println(startMessage);
-                userInput = scanner.nextInt();
-            } while (!(userInput >= 0 && userInput <= 100));
+        while (!(userValue.equalsIgnoreCase("Exit"))){
+            System.out.print("Please enter something: ");
+            userValue = scanner.nextLine();
         }
-        if(userInput>=90) {
-            System.out.println("Your grade is A!");
-        } else if(userInput>=80) {
-            System.out.println("Your grade is B!");
-        } else if(userInput>=70) {
-            System.out.println("Your grade is C!");
-        } else if(userInput>=60) {
-            System.out.println("Your grade is D!");
-        } else if(userInput>=50) {
-            System.out.println("Your grade is E!");
-        } else {
-            System.out.println("Your grade is F!");
-        }
+        System.out.println("You're entered: " + userValue);
 
 
-
+//task3
 
 
 
