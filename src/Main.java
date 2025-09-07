18ea7202c50df31a431e8d0b365b8aa69f36228a
yauheniya <evgeniya.boroznovskaya@gmail.com>
2025-09-08 00:25:15 +0300

    updated task1

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,37 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a number from 0 to 100: ");
+        String startMessage = "Please enter a number from 0 to 100:";
+        String warningMessage = "Incorrect value entered. ";
+
+        System.out.println(startMessage);
         int userInput = scanner.nextInt();
 
         if (userInput < 0 || userInput > 100) {
             do {
-                System.out.println("Incorrect value entered. ");
-                System.out.println("Please enter a number from 0 to 100: ");
+                System.out.println(warningMessage);
+                System.out.println(startMessage);
                 userInput = scanner.nextInt();
             } while (!(userInput >= 0 && userInput <= 100));
-        } else {
-            System.out.println("Bingo!");
         }
+        if(userInput>=90) {
+            System.out.println("Your grade is A!");
+        } else if(userInput>=80) {
+            System.out.println("Your grade is B!");
+        } else if(userInput>=70) {
+            System.out.println("Your grade is C!");
+        } else if(userInput>=60) {
+            System.out.println("Your grade is D!");
+        } else if(userInput>=50) {
+            System.out.println("Your grade is E!");
+        } else {
+            System.out.println("Your grade is F!");
+        }
+
+
+
+
+
 
 
     }
